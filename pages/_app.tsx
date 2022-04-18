b5b69703dee16129ff9f017b22eb4c04b2ee9d38
otera05 <otera05@gmail.com>
2022-04-19 00:49:58 +0900

    ✨ noindex 対応

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import 'tailwindcss/tailwind.css'
+import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Meta } from '../src/components/meta/index'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Meta></Meta>
+      <Component {...pageProps} />
+    </>
+  )
 }
 export default MyApp
