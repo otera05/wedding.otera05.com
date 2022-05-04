aff233ce027e62db04b485c6726404dd515dacb1
otera05 <otera05@gmail.com>
2022-05-04 12:06:28 +0900

    👍 ページタイトルの追加

--- src/components/meta/index.tsx
+++ src/components/meta/index.tsx
@@ -3,6 +3,7 @@ import Head from 'next/head'
 export const Meta = () => {
   return (
     <Head>
+      <title>Happiness Checker</title>
       <meta name="robots" content="noindex" />
     </Head>
   )
